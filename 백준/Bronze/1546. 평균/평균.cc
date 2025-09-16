29a701ef48603587c4e3c5db7ee71bd92bcbf87b
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 18:50:08 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	int arr[1000];
+	int max = 0;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+		if (arr[i] > max)
+			max = arr[i];
+	}
+
+	double sum = 0;
+	for (int j = 0; j < n; j++) {
+		sum = sum + ((double)arr[j] / max) * 100;
+	}
+
+	printf("%f \n", (double)sum / n);
+
+	return 0;
+
+}
