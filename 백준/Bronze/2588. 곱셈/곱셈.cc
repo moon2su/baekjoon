bf7066d82e9276229ea7f0e075f0566bd31ad944
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 19:01:13 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+
+	scanf("%d \n %d", &a, &b);
+
+	int c = a * (b % 10);
+	int d = a * (b / 10 % 10);
+	int e = a * (b / 100);
+
+	printf("%d\n", c);
+	printf("%d\n", d);
+	printf("%d\n", e);
+	printf("%d\n", e * 100 + d * 10 + c);
+
+	return 0;
+}
