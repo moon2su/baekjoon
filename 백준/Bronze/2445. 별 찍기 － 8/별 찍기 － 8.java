442902f06ac7fa2b54498216ae1cc3cfa146be5c
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 18:53:33 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 456 ms, Memory: 21472 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for(int i = 1; i <= N; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for (int k = 1; k <= 2 * N - 2 * i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int h = 1; h <= i; h++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+        for(int i = N-1; i >= 1; i--){
+
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+
+            for(int k = 1; k <= 2*N-2*i; k++){
+                System.out.print(" ");
+            }
+
+            for(int h = 1; h <= i; h++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
