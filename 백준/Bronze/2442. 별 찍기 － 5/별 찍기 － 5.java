a0204ac8b78d827a6f2b23ad1f9f19c70542bf39
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 18:52:54 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 372 ms, Memory: 19792 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for(int i = 1; i <= N; i++){
+
+            for(int k = 1; k <= N-i; k++){
+                System.out.print(" ");
+            }
+            
+            for(int j = 1; j <= 2*i-1; j++){
+                System.out.print("*");
+            }
+            
+            System.out.println(" ");
+        }
+    }
+}
