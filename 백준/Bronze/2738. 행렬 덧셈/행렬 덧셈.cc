d467886bdb0540d2be9fb825f26999631cd818c5
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 19:01:58 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+	int N, M, i, j;
+	scanf("%d %d", &N, &M);
+
+	int A[100][100], B[100][100];
+
+	for (i = 0; i < N; i++)
+		for (j = 0; j < M; j++)
+			scanf("%d", &A[i][j]);
+
+	for (i = 0; i < N; i++)
+		for (j = 0; j < M; j++)
+			scanf("%d", &B[i][j]);
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++) {
+			int sum = A[i][j] + B[i][j];
+			printf("%d ", sum);
+		}
+	printf("\n");
+
+	return 0;
+}
