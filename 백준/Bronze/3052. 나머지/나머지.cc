862f3a41f686615646b2cc50198b38cd203c3932
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 19:06:27 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+	int n, rst = 0;
+	int arr[10];
+
+	for (int i = 0; i < 10; i++) {
+		scanf("%d", &n);
+		arr[i] = n % 42;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		int count = 0;
+		for (int j = i + 1; j < 10; j++)
+			if (arr[i] == arr[j])
+				count++;
+		if (count == 0)
+			rst++;
+	}
+
+	printf("%d", rst);
+
+	return 0;
+
+}
