ed93631aeebbbd881055e072fcf75196750fba5a
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 19:11:36 +0900

    [Bronze III] Title: 최소, 최대, Time: 184 ms, Memory: 4900 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+	int N;
+	scanf("%d", &N);
+	int a[1000000];
+	int min = 1000000, max = -1000000;
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &a[i]);
+		if (a[i] > max)
+			max = a[i];
+		if (a[i] < min)
+			min = a[i];
+	}
+
+	printf("%d %d", min, max);
+
+	return 0;
+
+}
