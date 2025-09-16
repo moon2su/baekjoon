c79b1aa1953d144ccca32efd243259e0fa710249
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 18:49:38 +0900

    [Bronze I] Title: 2진수 8진수, Time: 1900 ms, Memory: 91724 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+
+        BigInteger N = new BigInteger(S, 2);
+
+        String result = N.toString(8);
+
+        System.out.println(result);
+
+
+    }
+}
