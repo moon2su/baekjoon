a2373a42c73ff96ab8bdf1192a009c6382f0b742
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 18:48:53 +0900

    [Bronze I] Title: 단어 공부, Time: 16 ms, Memory: 1968 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	char arr[1000000], cnt;
+	scanf("%s", arr);
+
+	int len, i, j, max = 0, alp[26];
+	len = strlen(arr);
+
+	for (i = 0; i < len; i++)
+		if (arr[i] >= 'a')
+			alp[arr[i] - 'a']++;
+		else
+			alp[arr[i] - 'A']++;
+
+	for (i = 0; i < 26; i++)
+		if (alp[i] == max)
+			cnt = '?';
+		else if (alp[i] > max) {
+			max = alp[i];
+			cnt = 'A' + i;
+		}
+
+	printf("%c", cnt);
+
+	return 0;
+
+}
