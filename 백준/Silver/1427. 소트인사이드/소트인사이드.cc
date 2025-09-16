c6e81a63f76e3804c54ad127e7ef794a0942dea0
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 18:49:54 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	int i, j;
+	char arr[10], a;
+	scanf("%s", arr);
+
+	for (i = 0; i < strlen(arr); i++) {
+		for (j = 0; j < strlen(arr) - 1; j++) {
+			if (arr[j] < arr[j + 1]) {
+				a = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = a;
+			}
+		}
+	}
+
+	printf("%s", arr);
+
+	return 0;
+
+}
