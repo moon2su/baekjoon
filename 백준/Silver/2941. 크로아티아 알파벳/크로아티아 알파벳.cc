dae9390b4f310496788d46dc1e427495ebbe6acb
moon2su <159042939+moon2su@users.noreply.github.com>
2025-09-16 19:05:34 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[100];
+    int i, count = 0;
+
+    scanf("%s", word);
+
+    for (i = 0; i < strlen(word); i++) {
+        count++;
+
+        if (word[i] == '=') {
+            if (word[i - 1] == 'c' || word[i - 1] == 's') {
+                count--;
+            }
+            if (word[i - 1] == 'z') {
+                count--;
+
+                if (word[i - 2] == 'd') {
+                    count--;
+                }
+            }
+        }
+
+        if (word[i] == '-') {
+            if (word[i - 1] == 'c' || word[i - 1] == 'd') {
+                count--;
+            }
+        }
+
+        if (word[i] == 'j') {
+            if (word[i - 1] == 'l' || word[i - 1] == 'n') {
+                count--;
+            }
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
