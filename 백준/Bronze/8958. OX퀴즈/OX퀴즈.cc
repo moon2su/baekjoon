29c84df7fe583779eec8a401b013df310d7611d4
moon2su <159042939+moon2su@users.noreply.github.com>
2026-01-02 14:32:47 +0900

    [Bronze II] Title: OX퀴즈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
-#include <string>
+using namespace std;
 
-int main() {
-	int N;
-	std::string s;
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    int N; cin >> N;
+    string S;
+    
+    for(int i = 0; i < N; i++){
+        cin >> S;
+        int cnt = 0;
+        int sum = 0;
+        
+        for(int j = 0; j < S.length(); j++){
+            if(S[j] == 'O'){ cnt++; }
+            else cnt = 0;
+            sum += cnt;
+        }
+        cout << sum << '\n';
+    }
 
-	std::cin >> N;
-	for (int i = 0; i < N; i++) {
-		std::cin >> s;
-		int sum, cnt;
-		sum = cnt = 0;
-		for (int j = 0; j < s.length(); j++) {
-			if (s[j] == 'O') cnt++;
-			else cnt = 0;
-			sum += cnt;
-		}
-		std::cout << sum << '\n';
-	}
+    return 0;
 }
