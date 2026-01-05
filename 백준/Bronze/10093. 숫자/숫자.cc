ca13838e153b16c1b1d839509962aa845bb8e810
moon2su <159042939+moon2su@users.noreply.github.com>
2026-01-06 00:03:22 +0900

    [Bronze II] Title: 숫자, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    long long A, B; cin >> A >> B;
+
+    if(A > B){
+        long long t;
+        t = B;
+        B = A;
+        A = t;
+    }
+    
+    if(A == B || B - A == 1){
+        cout << 0;
+    }
+    
+    else{
+        cout << B - A - 1 << '\n';
+        for(long long i = A + 1; i < B; i++){
+            cout << i << ' ';
+        }
+    }
+    
+    return 0;
+}
