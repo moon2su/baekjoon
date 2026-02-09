093a68b99a96b1a91624a1894635f433bce88015
moon2su <159042939+moon2su@users.noreply.github.com>
2026-02-09 19:41:15 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int N; cin >> N;
+    
+    for(int i = 0; i < N; i++){
+        
+        for(int k = i+ 1; k < N; k++){
+            cout << ' ';
+        }
+        
+        cout << '*';
+        
+        for(int j = 0; j < 2*i - 1; j++){
+            cout << ' ';
+        }
+        
+        if(i >= 1){
+            cout << '*';
+        }
+        
+        cout << '\n';
+    }
+    
+    return 0;
+}
