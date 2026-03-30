ae8e8d02b2980e4d56d6e316b0a0b8c218dbd84b
moon2su <159042939+moon2su@users.noreply.github.com>
2026-03-30 20:49:04 +0900

    [Gold IV] Title: 테트로미노, Time: 72 ms, Memory: 3240 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1, -1, 0, 0};
+int board[500][500];
+bool vis[500][500];
+int ans;
+
+void dfs(int y, int x, int d, int sum){
+    
+    sum += board[y][x];
+    vis[y][x] = 1;
+
+    if(d == 4){
+        if(sum > ans){
+            ans = sum;
+        }
+        vis[y][x] = 0;
+        return;
+    }
+    for(int dir = 0; dir < 4; dir++){
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if(nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
+        else{
+            if(!vis[ny][nx]){
+                dfs(ny, nx, d+1, sum);
+            }
+        }
+    }
+    vis[y][x] = 0;
+    
+}
+
+void T(int y, int x){
+    int sum, dir, ny, nx;
+    for(int i = 0; i < 4; i++){
+        sum = board[y][x];
+        for(dir = 0; dir < 4; dir++){
+            if(dir == i) continue;
+            ny = y + dy[dir];
+            nx = x + dx[dir];
+            if(ny >= 0 && ny < N && nx >= 0 && nx < M)
+                sum += board[ny][nx];
+        }
+        if(sum > ans)
+            ans = sum;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin >> board[i][j];
+        }
+    }
+    
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            dfs(i, j, 1, 0);
+            T(i, j);
+        }
+    }
+    
+    cout << ans;
+    
+    return 0;
+}
