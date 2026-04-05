e71144d9fa440cc64bf75b03aba05eaef4745dd2
moon2su <159042939+moon2su@users.noreply.github.com>
2026-04-05 20:00:17 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    while(1){
+        string str;
+        getline(cin, str);
+        
+        if(str == "."){
+            break;
+        }
+        
+        stack<char> S;
+        bool flag = 0;
+        
+        for(int i = 0; i < str.length(); i++){
+            char c = str[i];
+            
+            if((c == '(') || (c == '[')){
+                S.push(c);
+            }
+            else if(c == ')'){
+                if(!S.empty() && S.top() == '('){
+                    S.pop();
+                }
+                else{
+                    flag = 1;
+                    break;
+                }
+            }
+            else if(c == ']'){
+                if(!S.empty() && S.top() == '['){
+                    S.pop();
+                }
+                else{
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if(flag == 0 && S.empty()){
+            cout << "yes" << '\n';
+        }
+        else{
+            cout << "no" << '\n';
+        }
+    }
+    
+    return 0;
+}
